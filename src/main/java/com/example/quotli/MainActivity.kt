57c4a26f92ebb5411371f50cead2f5d975d6318a
dan-govier <dan.govier@gmail.com>
2020-12-06 22:09:41 -0600

    RecyclerView set up

--- src/main/java/com/example/quotli/MainActivity.kt
+++ src/main/java/com/example/quotli/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.quotli
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var sourcesList = mutableListOf<String>()
+    private var quotesList = mutableListOf<String>()
+    private var tagsList = mutableListOf<String>()
+
+    private var rv_recyclerView = findViewById<RecyclerView>(R.id.rv_recyclerView)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        postExamples()
+
+        rv_recyclerView.layoutManager = LinearLayoutManager(this)
+        rv_recyclerView.adapter = RecyclerAdapter(sourcesList, quotesList, tagsList)
     }
+
+
+
+    private fun addToList(source: String, quote: String, tags: String) {
+        sourcesList.add(source)
+        quotesList.add(quote)
+        tagsList.add(tags)
+    }
+
+    private fun postExamples() {
+        for (i in 1..5) {
+            addToList("Source $i", "Quote $i", "Tags $i")
+        }
+    }
+
+
 }
---
+++ src/main/java/com/example/quotli/RecyclerAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.quotli
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter (private var sources: List<String>, private var quotes: List<String>, private var tags: List<String>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val itemSource: TextView = itemView.findViewById(R.id.tv_source)
+        val itemQuote: TextView = itemView.findViewById(R.id.tv_quote)
+        val itemTags: TextView = itemView.findViewById(R.id.tv_tags)
+
+        init {
+            itemView.setOnClickListener { v: View ->
+                val position: Int = adapterPosition
+                Toast.makeText(
+                    itemView.context,
+                    "You clicked on item # ${position + 1}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        holder.itemSource.text = sources[position]
+        holder.itemQuote.text = sources[position]
+        holder.itemTags.text = sources[position]
+
+    }
+
+    override fun getItemCount(): Int {
+        return sources.size
+    }
+}
